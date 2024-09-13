538bad91092ea5280fde4c38a7c4fe20d2996505
JD <jaysondorilag1@gmail.com>
2024-09-13 23:01:44 +0800

    Add Tailwind CSS configuration file and dependencies

--- client/App.js
+++ client/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import Main from './Main';
 import store from './redux/store';
 
 export default function App() {
   return (
-    <Provider store = {store}>
-      <Main/>
+    <Provider store={store}>
+      <Main />
     </Provider>
   );
 }
-
--- client/Main.js
+++ client/Main.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
+import tw from 'twrnc';
 
-function Main() {
+const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <View style={tw`flex-1 justify-center items-center bg-white`}>
+    <Text style={tw`text-lg text-black`}>Main hehehehehe</Text>
+  </View>
+  );
+};
 
-export default Main
+export default Main;
