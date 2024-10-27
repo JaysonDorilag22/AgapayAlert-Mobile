c4c3b10528edeea64f731a0e2565cc3ba345ffae
JD <jaysondorilag1@gmail.com>
2024-10-27 14:49:54 +0800

    Refactor styles.js to remove unused code and update buttonPrimary and buttonSecondary styles

--- agapayAlert/src/styles/styles.js
+++ agapayAlert/src/styles/styles.js
@@ -7,19 +7,19 @@ export const colors = {
 };
 
 const styles = {
-  buttonPrimary: tw`bg-[#050C9C] text-white py-3 px-6 rounded w-3/4 m-2 border border-[#8BACB8]`,
-  buttonSecondary: tw`bg-[#F1FBFF] text-[#050C9C] py-3 px-6 rounded w-3/4 m-2 border border-[#8BACB8]`,
+  buttonPrimary: tw`bg-[#050C9C] text-white py-3 px-6  w-3/4 m-2 border border-[#8BACB8]`,
+  buttonSecondary: tw`bg-[#F1FBFF] text-[#050C9C] py-3 px-6  w-3/4 m-2 border border-[#8BACB8]`,
   buttonContent: tw`flex-row items-center justify-center`, 
   container: tw`flex-1 justify-center items-center bg-[#050C9C]`,
   text: tw`text-lg font-bold`,
   textWhite: tw`text-white font-semibold text-center`,
   textPrimary: tw`text-[#050C9C] font-semibold text-center`,
   icon: tw`w-70 h-70`, 
-  form: tw`w-full flex-1 bg-[#F1FBFF] items-center pt-10 rounded`,
-  form2: tw`w-full  bg-[#F1FBFF] items-center p-5 rounded`,
-  input: tw`py-2 px-4 rounded  w-3/4 m-1 border border-[#8BACB8]`,
+  form: tw`w-full flex-1 bg-[#F1FBFF] items-center pt-10 `,
+  form2: tw`w-full  bg-[#F1FBFF] items-center p-5 `,
+  input: tw`py-2 px-4   w-3/4 m-1 border border-[#8BACB8]`,
   textform: tw`text-left w-full ml-25 font-semibold`,
-  verificationInput: tw`border-2 rounded border-[#8BACB8] text-center w-15 h-15 font-bold p-2 m-2`,
+  verificationInput: tw`border-2  border-[#8BACB8] text-center w-15 h-15 font-bold p-2 m-2`,
   errorText: tw`ml-25 w-full text-red-500 text-sm`,
 };
 
