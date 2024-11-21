54a4b5edbbefbbe0eeb737e39e5a68d9941bd9cb
JD <jaysondorilag1@gmail.com>
2024-11-21 20:45:54 +0800

    Enhance PrivateRoute component to restrict access to admin users; implement navigation to Welcome screen for unauthorized access

--- agapayAlert/src/components/PrivateRoute.jsx
+++ agapayAlert/src/components/PrivateRoute.jsx
@@ -1,12 +1,20 @@
 // src/components/PrivateRoute.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 import { View, Text } from 'react-native';
 
-const PrivateRoute = ({ component: Component, roles, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
   const { user } = useSelector((state) => state.auth);
+  const navigation = useNavigation();
 
-  if (!user || !roles.includes(user.role)) {
+  useEffect(() => {
+    if (!user || user.role !== 'admin') {
+      navigation.navigate('Welcome');
+    }
+  }, [user, navigation]);
+
+  if (!user || user.role !== 'admin') {
     return (
       <View>
         <Text>Unauthorized Access</Text>
